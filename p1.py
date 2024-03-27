bc9bcbbdddbc8d8ed450b41747e00bb6f85f2afe
Stefa <stefanoperissutti2006@gmail.com>
2024-03-26 21:34:32 -0300

    Face detection parameters 0

--- p1.py
+++ p1.py
@@ -1,1 +1,22 @@
-print("hola mundo")
+import cv2
+import numpy
+
+faceClassif = cv2.CascadeClassifier("frontalface_classifier.xml")
+
+image = cv2.imread("oficina.jpeg")
+gray = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
+
+faces = faceClassif.detectMultiScale(gray,
+    scaleFactor=1.1,
+    minNeighbors=5,
+    minSize=(30, 30),
+    maxSize=(200, 200))
+
+print(faces)
+
+for (x,y,w,h) in faces:
+    cv2.rectangle(image, (x, y), (x+w, y+h), (0, 255, 0), 2)
+
+cv2.imshow("image", image)
+cv2.waitKey(0)
+cv2.destroyAllWindows()
