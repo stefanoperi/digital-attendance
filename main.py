73d783e27973f44d79c0f4527609d11b7acd0ef8
Stefano <stefanocuenta2006@gmail.com>
2024-07-31 20:29:25 -0300

    Refactorization of classes

--- main.py
+++ main.py
@@ -6,7 +6,6 @@ import face_recognition
 import sys
 import numpy as np
 
-
 from image_handling import face_utils as utils
 from database import db_module 
 from spreadsheet import spreadsheet_module 
@@ -44,8 +43,9 @@ def delete_faces_folder(student, face_manager):
    except OSError as e:
         raise OSError(f"Error deleting folder: {e}")
 
-class AppResources:
-    def __init__(self):
+class AppResources(App):
+    def __init__(self, **kwargs):
+        super().__init__(**kwargs)
         if not os.path.exists("image_handling/faces"):
             os.makedirs("image_handling/faces")
             print("New folder: faces")
@@ -164,93 +164,29 @@ class MainScreen(BoxLayout):
         self.clear_widgets()
         self.stop_update()
         self.add_widget(self.camera_layout)
-        self.add_widget(self.CapturerScreen(self))
+        capturer_screen = CapturerScreen(self)
+        self.add_widget(capturer_screen)
+        capturer_screen.on_enter()
 
     def stop_update(self):
         if self.update_event is not None:
             self.update_event.cancel()
             self.update_event = None
 
-    class CapturerScreen(BoxLayout):
-        def __init__(self, main_screen, **kwargs):
-            super().__init__(**kwargs)
-            self.main_screen = main_screen
-            self.change_previous_layout()
-            self.setup_forms()
-            
-        def change_previous_layout(self):
-            # Remove unnecesary widgets from previous layout
-            self.main_screen.camera_layout.remove_widget(self.main_screen.button_layout)
-            self.main_screen.camera_layout.remove_widget(self.main_screen.prompt_label)
-      
-        def setup_forms(self):
-            input_layout = BoxLayout(orientation='vertical', padding=10, spacing=10)
-            
-            # Name input
-            self.name_input = TextInput(hint_text='Name')
-            input_layout.add_widget(Label(text='Name'))
-            input_layout.add_widget(self.name_input)
-            
-            # Last name input
-            self.lastname_input = TextInput(hint_text='Last Name')
-            input_layout.add_widget(Label(text='Last Name'))
-            input_layout.add_widget(self.lastname_input)
-            
-            # ID input (only numbers)
-            self.id_input = TextInput(hint_text='ID', input_filter='int')
-            input_layout.add_widget(Label(text='ID'))
-            input_layout.add_widget(self.id_input) 
-                
-            # Action buttons
-            action_layout = BoxLayout(orientation='horizontal', spacing=10, size_hint_y=None, height=50)
-            
-            self.save_button = Button(text='Save', size_hint_x=0.2, width=50)
-            self.save_button.bind(on_press=self.save_action)
-            action_layout.add_widget(self.save_button)
-
-            self.cancel_button = Button(text='Cancel', size_hint_x=0.2, width=50)
-            self.cancel_button.bind(on_press=self.cancel_action)
-            action_layout.add_widget(self.cancel_button)
-            
-            # Add the input layout and action buttons to the main layout
-            input_layout.add_widget(action_layout)
-            self.main_screen.camera_layout.add_widget(input_layout)
-        
-        def save_action(self, instance):
-            # Handle the save action here
-            name = self.name_input.text
-            lastname = self.lastname_input.text
-            student_id = self.id_input.text
-            print(f"Saved: Name={name}, Last Name={lastname}, ID={student_id}")
-            # Additional save logic goes here
-
-        def cancel_action(self, instance):
-            # Handle the cancel action here
-            print("Cancelled")
-            # Additional cancel logic goes here
-        
-    
-
 class CapturerScreen(BoxLayout):
-    def __init__(self, transition_callback, resources, **kwargs):
+    def __init__(self, main_screen, **kwargs):
         super().__init__(**kwargs)
-        
-        # Initialize with transition callback and resources
-        self.transition = transition_callback
-        self.resources = resources
-        self.selected_course = None  
-        self.orientation = 'vertical'  
-        self.face_detector = utils.FaceDetector()
-        self.update_event = None
-
-    def on_enter(self, *args):
-        # Set up specific layouts and components when entering the screen
-        self.setup_ui()
-       
-    def setup_ui(self):
-        self.setup_camera_layout()
+        self.main_screen = main_screen
+    
+    def on_enter(self):
+        self.change_previous_layout()
         self.setup_forms()
-
+        
+    def change_previous_layout(self):
+        # Remove unnecesary widgets from previous layout
+        self.main_screen.camera_layout.remove_widget(self.main_screen.button_layout)
+        self.main_screen.camera_layout.remove_widget(self.main_screen.prompt_label)
+    
     def setup_forms(self):
         input_layout = BoxLayout(orientation='vertical', padding=10, spacing=10)
         
@@ -267,46 +203,35 @@ class CapturerScreen(BoxLayout):
         # ID input (only numbers)
         self.id_input = TextInput(hint_text='ID', input_filter='int')
         input_layout.add_widget(Label(text='ID'))
-        input_layout.add_widget(self.id_input)
-
-        self.add_widget(input_layout)
-    
-    def setup_camera_layout(self):
-        camera_layout = BoxLayout(orientation='vertical', size_hint=(0.8, 0.8), pos_hint={'center_x': 0.5, 'center_y': 0.5})
-        self.camera_placeholder = Label(text='Initializing camera...')
-        camera_layout.add_widget(self.camera_placeholder)
-        self.add_widget(camera_layout)
-
-        # Schedule camera initialization with a delay
-        Clock.schedule_once(self.initialize_camera, 10.0)
-    
-    def initialize_camera(self, dt):
-     
-        camera_index = 0
-        while camera_index < 5:  # Try up to 5 different indices
-            try:
-                self.camera = Camera(index=camera_index, play=True, size_hint=(1, 1))
-                self.camera_placeholder.parent.add_widget(self.camera)
-                self.camera_placeholder.parent.remove_widget(self.camera_placeholder)
-                return  # Exit if successful
-            except Exception as e:
-                self.camera_placeholder.text = f"Failed to initialize camera at index {camera_index}: {e}"
-                camera_index += 1
+        input_layout.add_widget(self.id_input) 
+            
+        # Action buttons
+        action_layout = BoxLayout(orientation='horizontal', spacing=10, size_hint_y=None, height=50)
         
-        # If no camera could be initialized, show an error message
-        self.camera_placeholder.text = 'Camera initialization failed for all indices'
+        self.save_button = Button(text='Save', size_hint_x=0.2, width=50)
+        self.save_button.bind(on_press=self.save_action)
+        action_layout.add_widget(self.save_button)
 
-    def update(self, dt):
-        # Method to update the camera display
-        image_texture = self.face_detector.live_comparison(self.resources.student_encodings, self.resources.last_registered_student, self.camera)
-        self.camera.texture = image_texture  # Update camera texture with live comparison result
+        self.cancel_button = Button(text='Cancel', size_hint_x=0.2, width=50)
+        self.cancel_button.bind(on_press=self.cancel_action)
+        action_layout.add_widget(self.cancel_button)
+        
+        # Add the input layout and action buttons to the main layout
+        input_layout.add_widget(action_layout)
+        self.main_screen.camera_layout.add_widget(input_layout)
+    
+    def save_action(self, instance):
+        # Handle the save action here
+        name = self.name_input.text
+        lastname = self.lastname_input.text
+        student_id = self.id_input.text
+        print(f"Saved: Name={name}, Last Name={lastname}, ID={student_id}")
+        # Additional save logic goes here
 
-    def stop_update(self):
-        # Method to stop the update schedule
-        if self.update_event is not None:
-            self.update_event.cancel()
-            self.update_event = None
-
+    def cancel_action(self, instance):
+        # Handle the cancel action here
+        print("Cancelled")
+        # Additional cancel logic goes here
 
 class DigitalAttendanceApp(App):
     def build(self):
@@ -318,26 +243,23 @@ class DigitalAttendanceApp(App):
    
         # Create the main screen and add it to the ScreenManager
         main_screen = Screen(name='main')
-        main_screen.add_widget(MainScreen(transition_callback=self.transition_to, resources=resources))
+        main_screen_widget = MainScreen(transition_callback=self.transition_to, resources=resources)
+        main_screen.add_widget(main_screen_widget)
         self.sm.add_widget(main_screen)
         
         # Create the capturer screen and add it to the ScreenManager
         capturer_screen = Screen(name='capturer')
-        capturer_screen_instance = CapturerScreen(transition_callback=self.transition_to, resources=resources)
+        capturer_screen_instance = CapturerScreen(main_screen=main_screen_widget)
         capturer_screen.add_widget(capturer_screen_instance)
         self.sm.add_widget(capturer_screen)
 
-        # Bind on_enter method of capturer_screen_instance to the on_enter event
-        capturer_screen.bind(on_enter=capturer_screen_instance.on_enter)
-
-        # Set the initial screen to the loading screen
+        # Set the initial screen to the main screen
         self.sm.current = 'main'
 
         return self.sm
 
     def transition_to(self, screen_name, *args):
         self.sm.current = screen_name
-
 
 if __name__ == '__main__':
     DigitalAttendanceApp().run()
---
+++ old.py
@@ -0,0 +1,100 @@
+""" 
+   
+
+def main(): 
+
+    if not os.path.exists("image_handling/faces"):
+            os.makedirs("image_handling/faces")
+            print("New folder: faces")
+    
+
+    face_manager = utils.FaceManager()
+    faces_path = "image_handling/faces"
+    
+    db_directory = "database"
+    db_file = "encodings.db"
+    db_path = os.path.join(db_directory, db_file)
+
+    os.makedirs(db_directory, exist_ok=True)
+
+    db_manager = db_module.DatabaseManager(db_path)
+    db_manager.connect()
+
+    student_encodings = {} 
+    new_photos = None
+    student = None
+    
+    
+    sheet_manager = spreadsheet_module.GoogleSheetManager("Toma de Asistencia") # Open by the google spreadsheet's name
+    worksheet_names = sheet_manager.read_worksheet_names() # Get course names
+    
+    # Ask for the grade to work with
+    if worksheet_names:
+        grade = get_valid_input(worksheet_names, f"Enter the grade for attendance (Available options: {worksheet_names}) ") 
+        sheet_manager.select_grade(grade)
+        print(f"Information of {grade}: {sheet_manager.read_values()}")
+    else:
+        raise ValueError("No courses are currently available")
+    
+
+    answer = None
+    while True:
+        answer = input("Do you want to add new photos? [yes/no] ").lower()
+
+        if answer == "yes":
+            capturer = utils.PhotoCapturer()
+            new_photos = capturer.capture_photo()
+
+            if new_photos:
+               full_name = input("Enter the full name of the captured person: ").strip().title()
+               student_id = input("Enter the ID number of the captured person: ")
+               student_grade = get_valid_input(worksheet_names, f"Enter the grade of the captured person (Available options: {worksheet_names}) ") 
+
+               student = Student(student_id=student_id, full_name=full_name, grade=student_grade)
+               # Ensure to delete remaining photos before adding new ones
+               delete_faces_folder(student, face_manager)
+
+               face_manager.save_faces(student, new_photos)
+               student_encodings = face_manager.encode_faces(faces_path, student)
+               break
+            else:
+                raise ValueError("No new photos have been taken")
+                
+            
+
+        elif answer == "no":
+            break
+    
+    db_manager.create_table()
+    mixed_encodings = db_manager.mix_encodings(student_encodings)
+    
+    face_detector = utils.FaceDetector()
+    face_detector.live_comparison(mixed_encodings, student)
+    answer = None
+   
+    # Save student data in the database for more efficient performance
+    if student_encodings:
+        while True:
+            answer = input("Do you want to save the person's data in the database? [yes/no]: ").lower()
+            
+            if answer == "yes":
+                db_manager.create_table()
+
+                # Insert encodings and information into the database
+                for student_id, encodings in student_encodings.items():
+                    for encoding in encodings:
+                        db_manager.insert_encoding(student, encoding.tobytes())
+
+                db_manager.close()
+                print("Data successfully saved to the database.")
+
+                sheet_manager.add_student(student)
+                break
+
+            elif answer == "no":
+                break
+        
+        delete_faces_folder(student, face_manager)
+
+if __name__ == "__main__":
+    main()"""
